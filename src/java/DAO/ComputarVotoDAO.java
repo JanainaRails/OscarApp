ddfc453c69ff1773a929399dd55f2edc4fa15b3a
JanainaRails <janaina.tads@outlook.com>
2017-11-18 05:29:53 -0200

    Arrumando ComputaVotoDAO

--- src/java/DAO/ComputarVotoDAO.java
+++ src/java/DAO/ComputarVotoDAO.java
@@ -8,15 +8,15 @@ public class ComputarVotoDAO {
     private final static String INSERE = "INSERT INTO votos(diretor, filme, id_usuario) VALUES (?, ?, ?)";
     private final static String VOTOU = "UPDATE usuario SET voto = 1 WHERE id = ?";
 
-    public boolean computarVoto(int diretor, int filme, int usuario){
+    public boolean computarVoto(String diretor, String filme, int usuario){
         Connection conn = null;
         PreparedStatement stmt = null;
         try{
             conn = new ConnectionFactory().getConnection();
             conn.setAutoCommit(false);
             stmt = conn.prepareStatement(INSERE);
-            stmt.setInt(1, diretor);
-            stmt.setInt(2, filme);
+            stmt.setString(1, diretor);
+            stmt.setString(2, filme);
             stmt.setInt(3, usuario);
             stmt.executeUpdate();
             
